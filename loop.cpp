61329b2ad231357e75014a789517c99f16276183
prashant patel <prashantpatel487330@gmail.com>
2026-03-07 22:11:30 +0530

    ..

---
+++ loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    // sum of natural numbers
+    int sum=0;
+    for(int i=1;i<=10;++i)
+    {
+      sum=sum+i;
+    }
+    cout<<sum;
+}
