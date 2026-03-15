b175c8a1e8739cdae6bd343d00d0b78d6b554457
prashant patel <prashantpatel487330@gmail.com>
2026-03-15 22:45:20 +0530

    ..

---
+++ p4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a=1;
+    for(int r=1;r<=3;r++)
+    {
+        for(int c=1;c<=r;c++)
+        {
+            cout<<a<<"\t";
+            a++;
+        }
+        cout<<endl;
+    }
+}
