32d05f8b0c74d05c3eedc7245abb388e8d46ce25
prashant patel <prashantpatel487330@gmail.com>
2026-03-18 22:34:13 +0530

    ..

---
+++ perfect.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    int n;
+    cout<<"enter the value of n ";
+    cin>>n;
+    while(i<=n)
+    {
+       if(i<n==n)
+       {
+        cout<<"perfect num";
+       }
+       else
+       {
+        cout<<"not perfect";
+       }
+    }
+}
