75814bf4ff9892d9847f91923ef42691d2f7a250
prashant patel <prashantpatel487330@gmail.com>
2026-03-26 21:09:04 +0530

    new

---
+++ pattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(char r='a';r<='c';r++)
+    {
+        for(char c='a';c<=r;c++)
+        {
+            cout<<c<<"\t";
+        }
+        cout<<"\n";
+    }
+}
