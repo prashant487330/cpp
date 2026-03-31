b65861be5c86f2e9222d01daa76989f8aa34520f
prashant patel <prashantpatel487330@gmail.com>
2026-03-31 22:24:35 +0530

    ..

---
+++ practice3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,count=0;
+    cin>>num;
+    for(int i=1;i<=num;i++)
+    {
+       if(num%i==0){
+        count++;
+       }
+       
+       }
+    if(count==2){
+       cout<<"Prime Num";
+    }
+    else{
+        cout<<"Not a prime";
+    }
+}
