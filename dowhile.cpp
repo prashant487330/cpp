6e3c863fb3ca4f45db342b93bcf65f4cef47a369
prashant patel <prashantpatel487330@gmail.com>
2026-04-01 20:23:49 +0530

    loop

---
+++ dowhile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // even num 10-0
+    int a=10;
+    do
+    {   if(a%2==0)
+        cout<<a<<"\n";
+        a--;
+    }
+    while(a>0);
+}
+// write a program to print a table of n numbers
+// write a program to display sum of the value 1-10
+// write a program to generate the square of each num 10- 20
