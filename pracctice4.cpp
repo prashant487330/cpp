91e1c2601c68957878bd497dc606662c8661e682
prashant patel <prashantpatel487330@gmail.com>
2026-04-02 22:15:37 +0530

    ..

---
+++ pracctice4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int fact,num=1;
+    cin>>fact;
+    for(int i=1;i<=fact;i++){
+        num=num*i;
+    }
+    cout<<"Factorial is:"<<num;
+}5
