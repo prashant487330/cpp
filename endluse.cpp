9babbf2c2fdbdf379d7eaf1e3c4fad6982ca7730
prashant patel <prashantpatel487330@gmail.com>
2026-04-11 22:31:39 +0530

    N:/

---
+++ endluse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"enter 2 numbers ";
+    cin>>a>>b;
+    cout<<"The multiplication of two no is :";
+    // endl is used to give next line
+    cout<<a*b<<endl;
+    cout<<"The subtraction of two no is :";
+    cout<<a-b<<endl;
+    cout<<"The division of two no is :";
+    cout<<a/b<<endl;
+    cout<<"The addition of two no is :";
+    cout<<a+b<<endl;
+}
