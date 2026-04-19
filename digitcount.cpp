6a17662938639b4aa4f26af02ef4320d71bb5f1e
prashant patel <prashantpatel487330@gmail.com>
2026-04-19 23:38:26 +0530

    ..

---
+++ digitcount.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num,c=1;
+   cout<<"Enter the num ";
+   cin>>num;
+   for(int i=0;i>0;i+1)
+   {
+      c=c+1;
+    num=num/10;
+   }
+   cout<<c;
+}
+
+
+// w.a.p to print the sum of the digit
+// w.a.p to print first and last digit
+// w.a.p to print sum of first and last digit
+// w.a.p to check the given no. is palindrome or not 
+// w.a.p to print revirse of digit 
