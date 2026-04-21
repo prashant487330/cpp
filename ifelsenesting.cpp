8173b25cbdb4a703a8e094e101ccdf624d31f23d
prashant patel <prashantpatel487330@gmail.com>
2026-04-21 22:06:17 +0530

    ..

---
+++ ifelsenesting.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int path;
+    cout<<"Enter the path";
+    cin>>path;
+    if(path==1)
+    {
+        cout<<"enter the path";
+        cin>>path;
+        if(path==2)
+        {
+            cout<<"Enter the path";
+            cin>>path;
+            if(path==3)
+            {
+                cout<<"reached";
+            
+            else
+            {
+                cout<<"invalid path";
+            }
+            // if(path==3)
+            // {
+            //     cout<<"Enter the path1";
+            //     cin>>path;
+            //     if(path==4)
+            //     {
+            //         cout<<"reached";
+            //     }else{
+            //         cout<<"invalid path";
+                }
+                
+                    }
+                    }
+                }
+            
+    
+          
+// else if(path==3)
+//  {
+//     cout<<"Enter the path";
+//     cin>>path;
+//     if(path==2)
+//     {
+//         cout<<"Enter the path";
+//         cin>>path;
+        
+//          if(path==4) {
+//             cout<<"rached";
+//          }else{
+//             cout<<"invalid input";
+//          }
+        
+//         }   
+//     }
+// }
+//     }
+// if(path==3)
+// {
+//     cout<<"Enter the path";
+//     cin>>path;
+//     if(path==4)
+//     {
+//         cout<<"Enter the path";
+//         cin>>path;
+//         else if(path==4)
+//         {
+//             cout<<"reached3";
+//             else
+//             {
+//                 cout<<"invalid path";
+//             }
+//         }
+    
+//     }
+// } 
+// if(path==4)
+// {
+//     cout<<"Enter the path";
+//     cin>>path;
+//     else if(path==4)
+//     {
+//         cout<<"reached4";
+//         else
+//         {
+//             cout<<"invalid path";
+//         }
+     
+ 
